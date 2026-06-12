4e7d52febfdd0f36766138fda3ec6bd150723fbf
Oluseye Lamina <oluseyelam@gmail.com>
2026-06-12 13:09:44 -0700

    first commit

---
+++ intro.js
@@ -0,0 +1,6 @@
+const name = "Oluseye Lamina";
+let age = 39; 
+let forvorite_prog_lang = "Javascript"
+
+console.log("My name is " + name + " and I am " + age + "my best programming lang is " + forvorite_prog_lang);
+
